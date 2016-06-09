c16d11ce20b410e5bf3741596a662b78910e2a96
alathans <alathans@users.noreply.github.com>
2016-06-09 18:57:24 -0500

    before fixing the data for credentials and licenses

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,5 +1,7 @@
 class License < ActiveRecord::Base
 
+has_many :states
+
 belongs_to :state , :class_name => "State", :foreign_key => "state_id"
 
 belongs_to :therapist , :class_name => "Therapist", :foreign_key => "therapist_id"
